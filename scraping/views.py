53b012f285044de7625b30e7382363338cdc3768
20390680 <20390680>
2022-11-29 21:39:45 +0300

    add models and html

--- scraping/views.py
+++ scraping/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Vacancy
+import datetime
 
-# Create your views here.
+
+def home_view(request):
+    vacancy = Vacancy.objects.all()
+    title = 'home'
+    date = datetime.datetime.now()
+    context = {
+        'vacancy': vacancy,
+        'title': title,
+        'date': date
+    }
+    return render(request, 'scraping/home.html', context=context)
--- scraping_service/views.py
+++ scraping_service/views.py
@@ -1,19 +1,1 @@
-import random
-
 from django.shortcuts import render
-import datetime
-
-
-def home(request):
-    date = datetime.datetime.now().date()
-    mood = ['good', 'bad', 'sad', 'happy']
-    rage = [i for i in range(20, 31)]
-    name = 'tema'
-    age = random.choice(rage)
-    context = {
-        'name': name,
-        'age': age,
-        'mood': random.choice(mood),
-        'date': date
-    }
-    return render(request, 'home.html', context=context)
