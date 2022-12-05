0b760b8a9418b5e5c8ea01e460803350a4374138
20390680 <20390680>
2022-12-05 14:15:25 +0300

    fix bag

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,8 @@
-from django.contrib import admin
-
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.core.exceptions import ValidationError
 
 from accounts.models import MyUser
 
@@ -18,14 +15,14 @@ class UserCreationForm(forms.ModelForm):
 
     class Meta:
         model = MyUser
-        fields = ('email',)
+        fields = ('email', )
 
     def clean_password2(self):
         # Check that the two password entries match
         password1 = self.cleaned_data.get("password1")
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
-            raise ValidationError("Passwords don't match")
+            raise forms.ValidationError("Passwords don't match")
         return password2
 
     def save(self, commit=True):
@@ -40,13 +37,19 @@ class UserCreationForm(forms.ModelForm):
 class UserChangeForm(forms.ModelForm):
     """A form for updating users. Includes all the fields on
     the user, but replaces the password field with admin's
-    disabled password hash display field.
+    password hash display field.
     """
     password = ReadOnlyPasswordHashField()
 
     class Meta:
         model = MyUser
         fields = ('email', 'password', 'is_active', 'is_admin')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]
 
 
 class UserAdmin(BaseUserAdmin):
@@ -57,10 +60,11 @@ class UserAdmin(BaseUserAdmin):
     # The fields to be used in displaying the User model.
     # These override the definitions on the base UserAdmin
     # that reference specific fields on auth.User.
-    list_display = ('email', 'is_admin')
+    list_display = ('email', 'is_admin', 'city', 'language', 'send_email')
     list_filter = ('is_admin',)
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
+        ('Vacancy', {'fields': ('city', 'language', 'send_email')}),
         ('Permissions', {'fields': ('is_admin',)}),
     )
     # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
